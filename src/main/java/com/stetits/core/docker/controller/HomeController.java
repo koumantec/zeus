7d64672dad0a8395baa761302acab1aae304e6b3
lonceny mara <pro.lmara@gmail.com>
2025-12-23 01:56:29 +0100

    update

--- src/main/java/com/stetits/core/docker/controller/HomeController.java
+++ src/main/java/com/stetits/core/docker/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.stetits.core.docker.controller;
 
+import com.stetits.core.docker.model.StackConfiguration;
 import com.stetits.core.docker.service.SettingsService;
+import com.stetits.core.docker.service.StackConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,11 +14,24 @@ public class HomeController {
     @Autowired
     private SettingsService settingsService;
 
+    @Autowired
+    private StackConfigService stackConfigService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
         if (!settingsService.isSettingsConfigured()) {
             return "redirect:/settings";
         }
+        
+        // Load stack configuration if available
+        StackConfiguration stackConfig = stackConfigService.loadStackConfiguration();
+        if (stackConfig != null) {
+            model.addAttribute("config", stackConfig);
+            model.addAttribute("hasStackConfig", true);
+        } else {
+            model.addAttribute("hasStackConfig", false);
+        }
+        
         return "index";
     }
 }
