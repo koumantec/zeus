8a23fd862075889ebf5292bb90586131fc46c00c
lonceny mara <pro.lmara@gmail.com>
2025-12-28 17:59:57 +0100

    update

--- src/main/java/com/stetits/core/domain/dto/StackVersionDto.java
+++ src/main/java/com/stetits/core/domain/dto/StackVersionDto.java
@@ -1,5 +1,13 @@
 package com.stetits.core.domain.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.Instant;
+import java.util.Map;
+
 public record StackVersionDto(
         @NotBlank String stackId,
         @NotBlank String version,
