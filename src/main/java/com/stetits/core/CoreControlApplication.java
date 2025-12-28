9c70c53a60beea1a798bbc5839018fbd496b984e
lonceny mara <pro.lmara@gmail.com>
2025-12-28 15:39:03 +0100

    initial commit

---
+++ src/main/java/com/stetits/core/CoreControlApplication.java
@@ -0,0 +1,13 @@
+package com.stetits.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreControlApplication.class, args);
+	}
+
+}
